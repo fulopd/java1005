40c105f3a804317d616303a6a15d06778a899d6f
Fülöp Dániel <fulopdaniel@gmail.com>
2019-10-05 10:21:50 +0200

    igy szebb

--- SzamBekerTizenegy/src/Main.java
+++ SzamBekerTizenegy/src/Main.java
@@ -3,27 +3,24 @@ import java.util.Scanner;
 public class Main {
 
 	static Scanner be = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
 
 		int szam = 0;
-		int szamlalo =0;
+		int szamlalo = 0;
+		try {
+			while (szam != 11) {
 
-		while(szam != 11) {
-			
-			System.out.print("Adj meg egy számot: ");
-			
-			try {
+				System.out.print("Adj meg egy számot: ");
+
 				szam = be.nextInt();
 				szamlalo++;
-				
-			} catch (Exception e) {
-				System.err.println("Nem számot adtál meg!");
+
 			}
-			
-			
+
+		} catch (Exception e) {
+			System.err.println("Nem számot adtál meg!");
 		}
-		
 		System.out.println("Siker: " + szamlalo);
 
 	}
